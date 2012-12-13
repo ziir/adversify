5b59bb5948acf8de4769daf4867503ad77dccfd1
tpillard <pillard@intechinfo.fr>
2012-12-13 15:13:17 +0100

    Added routes with session support

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,21 @@
  */
 
 exports.index = function(req, res){
-  res.render('index.html', { title: 'Step 1' });
+	if(req.session.user) {
+		if(req.session.kind == "publisher") {
+			res.redirect("/publisher/default");
+		} else if(req.session.kind == "advertiser") {
+			res.redirect("/advertiser/default");
+		}
+	}	
+  res.render('index.html', { title: 'Adversify - Sign Up' });
 };
 
 exports.pagenotfound = function(req,res){
 	res.render('404.html', { title: 'Page not found!'});
+}
+
+exports.logout = function(req, res){
+	req.session.destroy();
+	res.redirect("/");
 }
