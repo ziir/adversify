bc79132e45fdb999bcb107f6da6dd216414ada20
tpillard <pillard@intechinfo.fr>
2012-12-13 15:11:34 +0100

    Added Websites Category List Module

---
+++ modules/website-category-list.js
@@ -0,0 +1,16 @@
+
+module.exports = [
+    {short:"  " , name:"Website Main Category"},
+    {short:"NI" , name:"News & Informations"},
+    {short:"Co" , name:"Communities"},
+    {short:"EF" , name:"Economy & Finances"},
+    {short:"JT" , name:"Jobs & Training"},
+    {short:"CS" , name:"Computer Science"},
+    {short:"Ga" , name:"Games"},
+    {short:"HB" , name:"Health & Beauty"},
+    {short:"En" , name:"Entertainment"},
+    {short:"Te" , name:"Technology"},
+    {short:"SC" , name:"Shopping & Consumerism"},
+    {short:"Tr" , name:"Travel"},
+    {short:"Sp" , name:"Sports"}
+]
