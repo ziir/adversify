c42ac05141e518d9c726f8a9256fb96aff557def
tpillard <pillard@intechinfo.fr>
2012-12-19 02:15:32 +0100

    Publisher Manager with basic AddWebsite and GetWebsites REST style

--- modules/publisher-manager.js
+++ modules/publisher-manager.js
@@ -4,22 +4,53 @@
 
 */
 var mongoose = require('mongoose');
+var AM = require('../modules/account-manager.js');
 
 var PM = {};
 
 module.exports = PM;
 
-PM.addWebsite = function(user,newData,callback) {
-		var website = {
+PM.addWebsite = function(u,newData,callback) {
+		var w = new WebsiteModel({
 			"name":newData.sitename,
 			"category":newData.sitecategory,
 			"description":newData.sitedescription,
-			"url":newData.siteurl
-		};
-		var publisher; 
-		PublisherModel.findOne({username:user.username}, function(e,o) {
-			console.log(o);
-			o.update({websites:[website]},function(e) { console.log(e); });
-			callback(null,o);
+			"url":newData.siteurl,
+			"created":Date.now()
 		});
+		w.save(function(e,o){
+			if(!e) {
+				PublisherModel.findOneAndUpdate(
+				  { username:u },
+				  { $push: { websites: o }},
+				  { safe: true, upsert: true },
+				  function(e, o) {
+				    if(e) {
+				    	callback(e);
+				    } else {
+				    	callback(null,o);
+				    }
+				});	
+			} else {
+				callback(e);
+			}
+		});
+
+
+			//http://stackoverflow.com/questions/13412579/node-express-mongoose-sub-collection-document-insert?rq=1
 }
+
+PM.getWebsites = function(u,nb,sort,callback) {
+	PublisherModel.findOne({username:u}, function(e,o) {
+		if(!e) {
+			console.log(o.websites);
+			callback(null,o.websites);
+		} else {
+			callback(e);
+		}
+	})
+}
+
+PM.autoLogin = AM.autoLoginPublisher;
+
+PM.login = AM.loginPublisher;
