4c1b5049ce51e30d5542236762a60397eb8b548c
tpillard <pillard@intechinfo.fr>
2012-12-19 15:21:29 +0100

    Updated Ad Schema

--- appServer.js
+++ appServer.js
@@ -66,21 +66,21 @@ var Publisher = new Schema({
 });
 
 var Website = new Schema({
-    name: { type: String, required: true, unique: false }, 
-    url : { type : String, match : /((http:\/\/|https:\/\/)?(www.)?(([a-zA-Z0-9-]){2,}\.){1,4}([a-zA-Z]){2,6}(\/([a-zA-Z-_\/\.0-9#:?=&;,]*)?)?)/, required: true, unique: false },
+    name: { type: String, required: true }, 
+    url : { type : String, match : /((http:\/\/|https:\/\/)?(www.)?(([a-zA-Z0-9-]){2,}\.){1,4}([a-zA-Z]){2,6}(\/([a-zA-Z-_\/\.0-9#:?=&;,]*)?)?)/, required: true, unique: true },
     description : { type : String },
-    category : { type : String }, // TO DO : add Enum
+    category : { type : String }, 
     validated : { type : Boolean, default: false },
     modified: { type: Date, default: Date.now },
-    created: {type: Date},
+    created: {type: Date}
 });
 
 var Ad = new Schema({
     name: {type: String},
-    remuneration: [{ cp: {type: String},  repartition: {type: String} }],
-    kind: {type: String}, // TO DO : enum Image or textual
+    remuneration: [{ cp: {type: String ,enum: ['cpm', 'cpc']}, repartition: [{ cpm: {type:Number}, cpc: {type:Number}}] }],
+    kind: { type : String, enum: ['image','text'] }, 
     modified: {type: Date},
-    category: {type:String},
+    created: {type: Date},
     validated : { type : Boolean },
     colors : [{textColor: {type : String, default: "#333"}, borderColor: {type: String, default: "#000"}, bgColor: {type:String, default: "#fff"}, titleColor: {type:String, default:"#2672ec"}}] 
 });
