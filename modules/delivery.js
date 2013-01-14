6fb5536c0ad9067baaec74186762be7c656e94d6
tpillard <pillard@intechinfo.fr>
2013-01-14 02:14:58 +0100

    Minor addings to Delivery Module

--- modules/delivery.js
+++ modules/delivery.js
@@ -1,35 +1,18 @@
-var Hashids = require("hashids");
-var DV = {}; // Delivery Module, requests from publisher website
-/*var MongoClient = require('mongodb').MongoClient;
-MongoClient.connect("mongodb://localhost:27017/delivery", function(err, db) {
-  if(err) { return console.dir(err); }
- });*/
+var mongoose = require('mongoose');
+var DV = {}; // Delivery Module, requests from the client on the publisher website.
 module.exports = DV;
 
 
-DV.GenerateHashFromRandom = function(r,callback) {
-	console.log(r);
-  var hashids = new Hashids("this is my salt");
-  var hash = hashids.encrypt(Number(r));
-  callback(hash);
+DV.generateClientSideJS = function(publisherId,websiteId,randomString,callback) {
+  var script;
 
-//INSERT DANS BDD HASH VALIDE DEUX HEURES
-// CALLBACK 
-// RENVOI VALEUR AU CLIENT SANS MONGOOSE POPOPOP
-
-
-// Connect to the db
-/*
-
-  var collection = db.collection('test');
-  var doc1 = {'hello':'doc1'};
-  var doc2 = {'hello':'doc2'};
-  var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
-
-  collection.insert(doc1);
-
-  collection.insert(doc2, {w:1}, function(err, result) {});
-
-  collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
-*/
+  PublisherModel.findOne({_id:publisherId, "websites._id":websiteId}, function(e,o) {
+    if(o) {
+      callback(null,"Console.log: Site reconnu");
+    } else if(e) {
+      callback(e);
+    } else {
+      callback("Unexpected error occured");
+    }
+  });
 }
